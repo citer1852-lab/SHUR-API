fe1debabed0b28255f64672c571977c2d465aae3
citer1852-lab <citer1852@gmail.com>
2026-04-04 00:03:26 +0300

    Update sub.js

--- api/sub.js
+++ api/sub.js
@@ -1,18 +1,15 @@
-// api/sub.js
-const GITHUB_SUB_URL = 'https://citer1852-lab.github.io/SHUR-SUB/sub.txt'; // ваша ссылка
+const GITHUB_SUB_URL = 'https://citer1852-lab.github.io/SHUR-SUB/sub.txt';
 
 export default async function handler(req, res) {
   try {
     const response = await fetch(GITHUB_SUB_URL);
     const text = await response.text();
-    
-    // Заголовки, которые нужны HApp
+
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.setHeader('Content-Disposition', "attachment; filename*=utf-8''🧩 SHUR-SUB | NODE");
     res.setHeader('Profile-Update-Interval', '6');
-    // Статистика трафика и срок (замените expire на нужный Unix timestamp)
-    res.setHeader('Subscription-Userinfo', 'upload=0; download=0; total=0; expire=1735689600');
-    
+    res.setHeader('Subscription-Userinfo', 'upload=0; download=0; total=0; expire=1721011200');
+
     res.status(200).send(text);
   } catch (err) {
     res.status(500).send('Error fetching subscription');
